d8e02b1d2188d640cf0c3a7d613934c18c9bc345
Sergey <sergey.garbage@gmail.com>
2015-11-26 23:26:48 -0800

    1

--- cgi-bin/sensor.py
+++ cgi-bin/sensor.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python
 from Adafruit_BME280 import *
 import time
+import datetime
 from bokeh.plotting import figure
 from bokeh.io import vplot
 from bokeh.embed import components
-import sqlite3
 sensor = BME280(mode=BME280_OSAMPLE_8)
+from data import SQL
+sql=SQL()
 
-def sql_request(command):
-                connection=sqlite3.connect('base.sqlite')
-                curs = self.connection.cursor()
-                data=[]
-                try:
-                        for row in self.curs.execute(command):
-                                data.append(row)
-                except:
-                        data='None'
-                connection.commit()
-                connection.close()
-                return data
-
-
-
-
+today=str(datetime.datetime.today())[:10]
+yesterday=str(datetime.datetime.today()-datetime.timedelta(minutes=1440))[:10]
+x=[]
+a=0.5
+for i in range(0,47):
+	x.append(a)
+	a=a+0.5
 
 #xaxis = DatetimeAxis(formatter=DatetimeTickFormatter(formats={"months": ["%B %Y"], "days": ["%B %Y"]}),**AXIS_FORMATS)
 
@@ -55,36 +48,60 @@ hectopascals = pascals / 100
 humidity = sensor.read_humidity()
 mmHg=(pascals/100)*0.7600616827
 
+#------------------------------------------------------------------------------------------------------------
+temp_today=[]
+temp_yesterday=[]
 
-y1=[]
-temp=sql_request('SELECT sensor_data FROM stats WHERE sensor="temperature"')
+temp=sql.raw_request('SELECT sensor_data FROM stats WHERE sensor_name="temperature" AND time LIKE "'+today+'%"')
 for h in temp:
-	y1.append(h[0])
-x1=[x for x in range(0,len(y1))]
+	temp_today.append(h[0])
+temp=sql.raw_request('SELECT sensor_data, time FROM stats WHERE sensor_name="temperature" AND time LIKE "'+yesterday+'%"')
+for h in temp:
+	temp_yesterday.append(h[0])
 
-p1.line(x1, y1, color='red',line_width=4)
-p1.circle(x1, y1,  fill_color="white", size=8, color='red')
+
+
+p1.line(x, temp_yesterday, color='grey',line_width=1)
+p1.circle(x, temp_yesterday,  fill_color="white", size=4, color='grey')
+p1.line(x, temp_today, color='red',line_width=4)
+p1.circle(x, temp_today,  fill_color="white", size=8, color='red')
+p1.xaxis[0].ticker.desired_num_ticks=24
 p1.yaxis.axis_label_text_color="#FF0000"
-
-y2=[]
-temp=sql_request('SELECT sensor_data FROM stats WHERE sensor="humidity"')
+#-----------------------------------------------------------------------------------------------------------
+humidity_today=[]
+humidity_yesterday=[]
+temp=sql.raw_request('SELECT sensor_data FROM stats WHERE sensor_name="humidity" AND time LIKE "'+today+'%"')
 for h in temp:
-	y2.append(h[0])
-x2=[x for x in range(0,len(y2))]
+	humidity_today.append(h[0])
+temp=sql.raw_request('SELECT sensor_data FROM stats WHERE sensor_name="humidity" AND time LIKE "'+yesterday+'%"')
+for h in temp:
+	humidity_yesterday.append(h[0])
 
-p2.line(x2, y2, color='blue',line_width=4)
-p2.circle(x2, y2, fill_color="white", size=8)
+
+p2.line(x, humidity_yesterday, color='gray',line_width=1)
+p2.circle(x, humidity_yesterday, fill_color="white", size=4, color="gray")
+p2.line(x, humidity_today, color='blue',line_width=4)
+p2.circle(x, humidity_today, fill_color="white", size=8, color="blue")
+p2.xaxis[0].ticker.desired_num_ticks=24
 p2.yaxis.axis_label_text_color="#0000FF"
-
-y3=[]
-temp=sql_request('SELECT sensor_data FROM stats WHERE sensor="pressure"')
+#-----------------------------------------------------------------------------------------------------------
+pressure_today=[]
+pressure_yesterday=[]
+temp=sql.raw_request('SELECT sensor_data FROM stats WHERE sensor_name="pressure" AND time LIKE "'+today+'%"')
 for h in temp:
-	y3.append(h[0])
-x3=[x for x in range(0,len(y2))]
+	pressure_today.append(h[0])
+temp=sql.raw_request('SELECT sensor_data FROM stats WHERE sensor_name="pressure" AND time LIKE "'+yesterday+'%"')
+for h in temp:
+	pressure_yesterday.append(h[0])
 
-p3.line(x3, y3, color='green',line_width=4)
-p3.circle(x3, y3,  fill_color="white", size=8)
+
+p3.line(x, pressure_yesterday, color='grey',line_width=1)
+p3.circle(x, pressure_yesterday,  fill_color="white", size=4, color="grey")
+p3.line(x, pressure_today, color='green',line_width=4)
+p3.circle(x, pressure_today,  fill_color="white", size=8)
+p3.xaxis[0].ticker.desired_num_ticks=12
 p3.yaxis.axis_label_text_color="#00FF00"
+#-----------------------------------------------------------------------------------------------------------
 m=vplot(p1,p2,p3)
 script, div = components(m)
 
@@ -103,7 +120,6 @@ print top
 print script
 print '</head>'
 print '<body>'
-
 print '<table width="100%">'
 print '<tr>'
 print '<th rowspan="3" style="width: 1000px;">'
