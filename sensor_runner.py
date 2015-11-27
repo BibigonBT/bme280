016649d0fc740fc270525e5f27780aa086b3c605
Sergey <sergey.garbage@gmail.com>
2015-11-26 23:45:33 -0800

    1

--- sensor_runner.py
+++ sensor_runner.py
@@ -3,8 +3,7 @@ import os
 import sqlite3
 import datetime
 from Adafruit_BME280 import *
-import time
-
+import random
 
 class SQL():
         def __init__(self,debug=1):
@@ -12,8 +11,7 @@ class SQL():
                         self.connection=sqlite3.connect('base.sqlite')
                         self.curs = self.connection.cursor()
                         tblcmd =[]
-                        #tblcmd.append('CREATE TABLE tests (test_id char(20) NOT NULL, day char(20) NOT NULL,  PRIMARY KEY (test_id))')
-                        tblcmd.append('CREATE TABLE stats ( sensor char(50) NOT NULL, sensor_data float(500) NOT NULL,  day char(20) NOT NULL, time char(20) NOT NULL)')
+                        tblcmd.append('CREATE TABLE stats ( "sensor_location" char(50) NOT NULL, "sensor_name" char(50) NOT NULL, "sensor_data" float(30) NOT NULL,  "time" DATETIME NOT NULL)')
                         for cmd in tblcmd:
                                 self.curs.execute(cmd)
         def connect(self):
@@ -23,18 +21,14 @@ class SQL():
         def flush_table(self):
                 self.connect()
                 self.curs.execute('DELETE FROM stats ')
-                #self.curs.execute('DELETE FROM tests ')
                 self.connection.commit()
                 self.connection.close()
 
         def add_data(self,temperature,pressure,humidity):
-                day=str(datetime.datetime.today())[:10]
-                time=datetime.datetime.now().time()
-                time=str(time.replace(microsecond=0))
-
-                self.raw_request('INSERT INTO stats VALUES ("temperature", "' +str(temperature) +'", "' + day + '", "' + time + '")')
-                self.raw_request('INSERT INTO stats VALUES ("pressure", "' + str(pressure) +'", "' + day + '", "' + time + '")')
-                self.raw_request('INSERT INTO stats VALUES ("humidity", "' + str(humidity) +'", "' + day + '", "' + time + '")')
+                time=str(datetime.datetime.today())
+                self.raw_request('INSERT INTO stats VALUES ("Room" , "temperature", "' + str(temperature) +'", "' + time + '")')
+                self.raw_request('INSERT INTO stats VALUES ("Room" , "pressure", "'    + str(pressure)    +'", "' + time + '")')
+                self.raw_request('INSERT INTO stats VALUES ("Room" , "humidity", "'    + str(humidity)    +'", "' + time + '")')
 
 
         def raw_request(self,command):
@@ -49,16 +43,37 @@ class SQL():
                 self.connection.close()
                 return data
 
+	def fake_data(self):
+		dates_delta=4320 # 3 days
+		finish=datetime.datetime.today()
+		start=finish-datetime.timedelta(minutes=dates_delta)
+		delta=0
+		for i in range(0,144):
+			
+			time=start+datetime.timedelta(minutes=delta)
+			self.raw_request('INSERT INTO stats VALUES ("Room" , "temperature", "' + str(random.randrange(0,35)) +'", "' + str(time) + '")')
+                	self.raw_request('INSERT INTO stats VALUES ("Room" , "pressure", "'    + str(random.randrange(750,770))    +'", "' + str(time) + '")')
+                	self.raw_request('INSERT INTO stats VALUES ("Room" , "humidity", "'    + str(random.randrange(30,80))    +'", "' + str(time) + '")')
+			delta=delta+30
+			
+	def main_loop(self):
+		while True:
+			if datetime.datetime.now().minute==00 or datetime.datetime.now().minute==30: #check sensors every 30 minutes
+				degrees = sensor.read_temperature()
+				pascals = sensor.read_pressure()
+				hectopascals = pascals / 100
+				humidity = sensor.read_humidity()
+				mmHg=(pascals/100)*0.7600616827
+				sql.add_data(degrees,mmHg,humidity)
+				time.sleep(1500)
+			time.sleep(10)
+
 sql=SQL()
 sensor = BME280(mode=BME280_OSAMPLE_8)
-print sql.raw_request('SELECT sensor_data FROM stats ')
-while True:
-	if datetime.datetime.now().minute==0 or datetime.datetime.now().minute==30: #check sensors every 30 minutes
-		degrees = sensor.read_temperature()
-		pascals = sensor.read_pressure()
-		hectopascals = pascals / 100
-		humidity = sensor.read_humidity()
-		mmHg=(pascals/100)*0.7600616827
-		sql.add_data(degrees,mmHg,humidity)
-		time.sleep(30)
+
+#sql.fake_data()
+#print sql.raw_request('SELECT * FROM stats ')
+#print sql.raw_request('Select time FROM stats WHERE sensor_name="temperature" AND time LIKE "2015-11-26%"')
+sql.main_loop()
+
 
